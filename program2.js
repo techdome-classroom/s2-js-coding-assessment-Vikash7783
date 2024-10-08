5719799e2e0fdd3211cdad36aef1fe6468e94a84
VIKASH KUMAR <vikashkumarbuxar419@gmail.com>
2024-10-08 14:35:31 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,37 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    // Mapping of Roman numerals to their integer values
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
+    let total = 0;
+    let prevValue = 0;
 
-module.exports={romanToInt}
+    // Iterate through the Roman numeral from right to left
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanMap[s[i]];
+
+        // If the current value is less than the previous value, subtract it
+        if (currentValue < prevValue) {
+            total -= currentValue;
+        } else {
+            total += currentValue;
+        }
+
+        // Update the previous value to the current value
+        prevValue = currentValue;
+    }
+
+    return total;
+}
+
+// Example usage
+console.log(romanToInt("III"));       // Output: 3
+console.log(romanToInt("LVIII"));     // Output: 58
+console.log(romanToInt("MCMXCIV"));   // Output: 1994
